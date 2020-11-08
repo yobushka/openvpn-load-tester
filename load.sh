cd5420cb88b1a20207b0bc1176c244b27de951e4
yobushka <51312852+yobushka@users.noreply.github.com>
2020-11-08 21:38:35 +0300

    Update load.sh
    added parallel limits

--- load.sh
+++ load.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export DOCKER_CLIENT_TIMEOUT=360
 export COMPOSE_HTTP_TIMEOUT=360
+export COMPOSE_PARALLEL_LIMIT=1000
 docker-compose down
 docker build -t yoba .
 docker-compose up --scale yoba=300 -d
