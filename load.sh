3027683d7f6f39947a67867cf1a59f7ad77009dc
yobushka <51312852+yobushka@users.noreply.github.com>
2020-11-08 21:26:10 +0300

    Update load.sh
    added prune,

--- load.sh
+++ load.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+export DOCKER_CLIENT_TIMEOUT=360
+export COMPOSE_HTTP_TIMEOUT=360
+docker-compose down
 docker build -t yoba .
-docker-compose up --scale yoba=10 -d
+docker-compose up --scale yoba=300 -d
